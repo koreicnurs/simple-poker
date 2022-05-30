06d825b7affee821109035cf4412ef36dbcf52b3
koreicnurs <koreicnurs@gmail.com>
2022-05-30 18:20:01 +0600

    add class CardDeck which create deck

---
+++ src/CardDeck.js
@@ -0,0 +1,32 @@
+class CardDeck {
+    constructor() {
+        this.suits = ["hearts", "spades", "clubs", "diams"];
+        this.ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+        this.cardDeck = [];
+        this.getRandomCards = [];
+        for (let s = 0; s < this.suits.length; s++) {
+            for (let r = 0; r < this.ranks.length; r++) {
+                let card = { suit: this.suits[s], rank: this.ranks[r] };
+                this.cardDeck.push(card);
+            }
+        }
+    }
+
+    getCard() {
+        const randomCard = this.cardDeck[Math.floor(Math.random() * this.cardDeck.length)]
+        this.cardDeck.splice(this.cardDeck.indexOf(randomCard), 1);
+        return randomCard;
+    }
+
+    getCards(howMany) {
+        let i = 0;
+
+        while (i < howMany) {
+            this.getRandomCards.push(this.getCard());
+            i++;
+        }
+        return this.getRandomCards
+    }
+}
+
+export default CardDeck;
